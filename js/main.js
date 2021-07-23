3c41da2d156cb06ce1de9ed0a653214b514b66b8
millennium0ne <cbrooks86@gmail.com>
2021-07-23 17:18:59 -0400

    Changes

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@
   const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Beliefs. Actions. Results. "];
+const textArray = ["Increased Following.", "Trust and Loyalty.", "A Deeper Understanding.", "Momentum."];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
